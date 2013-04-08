3556dd50a9bb30546a3644bdf67cc831cf1d2792
vlewin <vlewin@suse.de>
2013-04-08 17:26:31 +0200

    load config data from local file if exist

--- trello2wr.rb
+++ trello2wr.rb
@@ -4,10 +4,17 @@ require 'trello'
 require 'yaml'
 require 'uri'
 
-CONFIG = YAML.load_file(File.expand_path(File.dirname(File.dirname(__FILE__)), 'config.yml'))
+
+if File.exist? File.expand_path(File.dirname(File.dirname(__FILE__)), 'config-local.yml')
+  CONFIG = YAML.load_file(File.expand_path(File.dirname(File.dirname(__FILE__)), 'config-local.yml'))
+else
+  CONFIG = YAML.load_file(File.expand_path(File.dirname(File.dirname(__FILE__)), 'config'))
+end
 
 class Workreport
   attr_reader :config, :user, :year, :week
+
+  @@debug = false
 
   def initialize
     @year = Date.today.year
@@ -17,15 +24,20 @@ class Workreport
 
   # Get Trello cards
   def sign_in
+    self.log("*** Trello API basic authorization")
+
     Trello.configure do |config|
       config.developer_public_key = CONFIG["default"]["trello_developer_public_key"]
       config.member_token = CONFIG["default"]["trello_member_token"]
     end
 
+    self.log("*** Find user: #{CONFIG["default"]["trello_username"]}")
     Trello::Member.find(CONFIG["default"]["trello_username"])
   end
 
   def done
+    self.log("*** Get 'Done' cards")
+
     last_week = self.week-1
     done = self.user.boards.first.lists.find{|l| l.name == "Done (#{self.year}##{last_week})"}
     cards = done.cards.select{|card| card.member_ids.include? self.user.id}
@@ -33,14 +45,17 @@ class Workreport
   end
 
   def doing
-    done = self.user.boards.first.lists.find{|l| l.name == "Doing"}
-    cards = done.cards.select{|card| card.member_ids.include? self.user.id}
+    self.log("*** Get 'Doing' cards")
+
+    doing = self.user.boards.first.lists.find{|l| l.name == "Doing"}
+    cards = doing.cards.select{|card| card.member_ids.include? self.user.id}
     cards.map{|c| "- #{c.name.downcase} [WIP]\n"}
   end
 
   def todo
-    done = self.user.boards.first.lists.find{|l| l.name == "To Do"}
-    cards = done.cards.select{|card| card.member_ids.include? self.user.id}
+    self.log("*** Get 'To Do' cards")
+    todo = self.user.boards.first.lists.find{|l| l.name == "To Do"}
+    cards = todo.cards.select{|card| card.member_ids.include? self.user.id}
     cards.map{|c| "- #{c.name.downcase}\n"}
   end
 
@@ -70,7 +85,15 @@ class Workreport
 
   def prepare
     mailto = self.construct_mail_to_url(CONFIG["default"]["recipient"], self.subject, self.body)
+
+    self.log("*** Format e-mail and open e-mail client")
+
+    # TODO: add support for another e-mail clients
     system("thunderbird #{mailto}")
+  end
+
+  def log(message)
+    puts message if @@debug
   end
 end
 
